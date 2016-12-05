73eb197a6636aa542671f371aa0db31a3d1ddccf
rtevonian <reidtevonian@gmail.com>
2016-12-04 18:28:41 -0600

    Add files via upload

---
+++ Client.java
@@ -0,0 +1,74 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JTextArea;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Client {
+	protected String ClientId=null;
+	private JFrame frame;
+	private JTextField textField;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Client window = new Client();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Client() {
+		initialize();
+		this.frame.setVisible(false);
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 230, 259, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(10, 0, 414, 217);
+		frame.getContentPane().add(textArea);
+		
+		JButton btnNewButton = new JButton("Leave");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				close();
+			}
+		});
+		btnNewButton.setBounds(289, 229, 114, 23);
+		frame.getContentPane().add(btnNewButton);
+	}
+	public void showClient(){
+		this.frame.setVisible(true);
+	}
+	public void close(){
+		this.frame.dispose();
+		MainServer.showServer();
+	}
+}
